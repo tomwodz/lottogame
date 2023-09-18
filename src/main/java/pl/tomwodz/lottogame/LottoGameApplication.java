187e27e8c9f2c405adb71ca106931fff6e6737b7
twodzinski@op.pl <twodzinski@op.pl>
2023-09-18 10:23:18 +0200

    added configuration mongoDB, spring data mongoDB

--- src/main/java/pl/tomwodz/lottogame/LottoGameApplication.java
+++ src/main/java/pl/tomwodz/lottogame/LottoGameApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LottoGameApplication {
+
+
     public static void main(String[] args) {
         SpringApplication.run(LottoGameApplication.class);
     }
