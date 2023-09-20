40a6ad2ddff0dfbc837e3cb7f95c5792f9fd245c
twodzinski@op.pl <twodzinski@op.pl>
2023-09-20 17:51:47 +0200

    added swagger

--- src/integration/java/pl/tomwodz/lottogame/feature/UserPlayedLottoAndWonIntegrationTest.java
+++ src/integration/java/pl/tomwodz/lottogame/feature/UserPlayedLottoAndWonIntegrationTest.java
@@ -23,6 +23,7 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
 public class UserPlayedLottoAndWonIntegrationTest extends BaseIntegrationTest {
 
     @Autowired
