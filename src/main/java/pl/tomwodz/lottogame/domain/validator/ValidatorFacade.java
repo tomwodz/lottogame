ecfed2818fcf7551d24a37fbd391e84373515264
twodzinski@op.pl <twodzinski@op.pl>
2023-09-24 21:13:23 +0200

    add configuration properties to validator

--- src/main/java/pl/tomwodz/lottogame/domain/validator/ValidatorFacade.java
+++ src/main/java/pl/tomwodz/lottogame/domain/validator/ValidatorFacade.java
@@ -1,6 +1,7 @@
 package pl.tomwodz.lottogame.domain.validator;
 
 import lombok.AllArgsConstructor;
+import pl.tomwodz.lottogame.domain.numbergenerator.dto.CriteriaForGenerateNumbersConfigurationProperties;
 
 import java.util.Set;
 
@@ -9,14 +10,14 @@ public class ValidatorFacade {
 
     private final ValidatorNumber validatorNumber;
     private final ValidatorWinningNumbers validatorWinningNumbers;
-
+    CriteriaForGenerateNumbersConfigurationProperties criteria;
 
     public boolean validationNumbers(Set<Integer> numbersFromUser){
-        return validatorNumber.areAllNumbersInRange(numbersFromUser);
+        return validatorNumber.areAllNumbersInRange(numbersFromUser, criteria);
     }
 
     public Set<Integer> validationWinningNumbers(Set<Integer> winningNumbers){
-        return validatorWinningNumbers.validate(winningNumbers);
+        return validatorWinningNumbers.validate(winningNumbers, criteria);
     }
 
 
